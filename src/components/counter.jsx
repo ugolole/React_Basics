125f1032580c5eb43e38ad1d571d9783a0666ead
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-03-17 05:25:21 -0400

    Adding a counter component.

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+// Create a counter class that extends the component class
+// The component class contains a set of methods that are needed by the Counter class.
+class Counter extends Component {
+
+    render(){
+        const element = <h1>This is counter element</h1>
+        return (element);
+    }
+}
+
+export default Counter;
