a8bd43fd91d8b2f23f3f492bbaa7e8c209d9615c
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-03-17 23:53:11 -0400

    Adding code used for the couter compoment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 // Create a counter class that extends the component class
 // The component class contains a set of methods that are needed by the Counter class.
 class Counter extends Component {
+  state = {
+    count: 0,
+    image: "https://picsum.photos/200",
+  };
 
-    render(){
-        const element = <h1>This is counter element</h1>
-        return (element);
-    }
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold",
+  };
+
+  imageStyle = {
+    borderRadius: 30,
+  };
+
+  render() {
+    return (
+      <div>
+        <img style={this.imageStyle} src={this.state.image} alt="" />
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state; // this is a destructor object used to make code easier to understand.
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
