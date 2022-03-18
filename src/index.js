415eb7d7de8d4d7be1055bd53e71dc9a5ebd86be
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-03-17 23:53:27 -0400

    update index.js to contain the latest code.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Counter from './components/counter';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Counter from "./components/counter";
 
-for(var i =0; i<3; i++){
+for (var i = 0; i < 3; i++) {
   console.log("This happened ", i);
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
-    <Counter/>
+    <Counter />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
