ffe9c41dd78e958aaa10f9b2d30454268858ae78
Ivan Richard Ugolole <30692799+ugolole@users.noreply.github.com>
2022-05-10 16:16:17 -0400

    Adding coppoLog component to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Counter from "./components/counter";
+import CoppoLogs from "./components/coppoLogs";
 
 for (var i = 0; i < 3; i++) {
   console.log("This happened ", i);
@@ -11,7 +12,7 @@ for (var i = 0; i < 3; i++) {
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counter /> <CoppoLogs/>
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -20,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+    
